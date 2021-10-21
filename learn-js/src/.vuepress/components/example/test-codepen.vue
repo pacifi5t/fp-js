ca1f55ce71d4642947be2aa81b8bb87a70dfe5ae
valerachuk <valerachuk@gmail.com>
2021-10-21 12:02:34 +0300

    #12: Implemented basic app structure, added Codepen examples

---
+++ learn-js/src/.vuepress/components/example/test-codepen.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p
+      class="codepen"
+      data-height="700"
+      data-theme-id="light"
+      data-default-tab="js,result"
+      data-slug-hash="bGrwYXZ"
+      data-editable="true"
+      data-user="valerachuk"
+      style="
+        height: 700px;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid;
+        margin: 1em 0;
+        padding: 1em;
+      "
+    >
+      <span
+        >See the Pen
+        <a href="https://codepen.io/valerachuk/pen/bGrwYXZ"> MyFirstCodepen</a>
+        by Valerii (<a href="https://codepen.io/valerachuk">@valerachuk</a>) on
+        <a href="https://codepen.io">CodePen</a>.</span
+      >
+    </p>
+    <script
+      async
+      src="https://cpwebassets.codepen.io/assets/embed/ei.js"
+    ></script>
+  </div>
+</template>
