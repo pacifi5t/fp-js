8f72a7ac56884ce9f221bd2d827d2a0425f20bee
valerachuk <valerachuk@gmail.com>
2021-10-26 23:19:40 +0300

    #21: Created projects. Implemented basic layout

---
+++ fsharp-snippets/FSharpPlots/Core/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace Core
+
+open System.Windows.Controls
+open System.Windows.Shapes
+open System.Windows.Media
+
+module PlotBuilder1 =
+  let Build (canvas: Canvas) = 
+    let ellipse = Ellipse(Width = 100., Height = 50., Fill = SolidColorBrush(Colors.Red))
+    canvas.Children.Add(ellipse)
